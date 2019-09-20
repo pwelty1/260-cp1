06148c4d91bd86973080cd55c5c58bf0d50bbf84
Peter Welty <weltypeter@gmail.com>
2019-09-20 10:10:03 -0600

    new stuff

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
-let data
+
+async function loadData() {
+  const data = await fetch("data.json").then(res => res.json());
+
+  const mason = document.querySelector(".masonry");
+
+  mason.innerHTML = data
+    .map(({ file }) => {
+      return `<img class="item" src="images/${file}">`;
+    })
+  .join("");
+}
 
 document.body.onload = async () => {
-	data = await fetch('data.json')
-		.then(res => res.json())
-}
+  loadData();
+};
